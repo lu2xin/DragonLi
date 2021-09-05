df44e172d48961e5d3c8928181273935a3bc626e
lu2xin <47627985+imRobots@users.noreply.github.com>
2021-09-06 02:04:09 +0800

    暂时精简部分依赖

---
+++ packages/renderer/src/components/DSearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Hi, DragonLi!</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    setup() {
+
+        return {
+        };
+    },
+});
+</script>
+
+<style>
+</style>
